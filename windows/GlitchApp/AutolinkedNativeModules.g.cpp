c0073e494d668c1fd56773d019df2673e6fefd89
mackenzie <mackenzie@yourmoms>
2023-03-10 17:31:27 -0600

    feat: add react-native-document-picker

--- windows/GlitchApp/AutolinkedNativeModules.g.cpp
+++ windows/GlitchApp/AutolinkedNativeModules.g.cpp
@@ -3,6 +3,9 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-document-picker
+#include <winrt/ReactNativeDocumentPicker.h>
+
 // Includes from react-native-fs
 #include <winrt/RNFS.h>
 
@@ -11,6 +14,8 @@ namespace winrt::Microsoft::ReactNative
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
+    // IReactPackageProviders from react-native-document-picker
+    packageProviders.Append(winrt::ReactNativeDocumentPicker::ReactPackageProvider());
     // IReactPackageProviders from react-native-fs
     packageProviders.Append(winrt::RNFS::ReactPackageProvider());
 }
